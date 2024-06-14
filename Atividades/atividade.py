4db326479347ded7991ade71d92fc292e021c7d7
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2024-06-14 13:18:58 -0300

    Aula 3 - Atividade 1
    primeira atividade do curso

---
+++ Atividades/atividade.py
@@ -0,0 +1,30 @@
+# Aula 3 - Lista de Exercícios
+
+### Nome: Saul Alves Martins de Oliveira
+
+### Turma: 1ª Turma
+
+###OBS: Exercício feito em python
+
+1. fruta = maca
+2. numeros = [1,  2, 3, 4, 5]
+3. print ["numeros[2]"]
+4. matriz = [
+[1, 2],
+[3, 4]
+]
+5. verdade = True
+6. nomes = ["Joao", "Maria", "Pedro"] 
+7. print (nomes[1])
+8. matriz2 = [
+
+[1, 2, 3]
+
+[4, 5, 6]
+
+[7, 8, 9]
+
+]
+
+1. numero = 3.14
+2. letras = ['a', 'b', 'c']
