5b232615da2ceaa24b4f22dec9f97e583e955e05
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2024-06-28 17:08:42 -0300

    atividade 3 - swift
    mudança da linguagem da atividade para swift.

---
+++ Atividades/atividade.swift
@@ -0,0 +1,34 @@
+# Aula 3 - Lista de Exercícios
+
+### Nome: Saul Alves Martins de Oliveira
+
+### Turma: 1ª Turma
+
+Definindo uma variável de string
+var fruta = "maçã"
+
+1. Definindo um array de números
+var numeros = [1, 2, 3, 4, 5]
+2. // Imprimindo o terceiro elemento do array (índice 2)
+print(numeros[2])
+3. // Definindo uma matriz 2x2 (array de arrays)
+var matriz = [
+[1, 2],
+[3, 4]
+]
+4. // Definindo uma variável booleana
+var verdade = true
+5. // Definindo um array de nomes
+var nomes = ["Joao", "Maria", "Pedro"]
+6. // Imprimindo o segundo elemento do array (índice 1)
+print(nomes[1])
+7. // Definindo uma matriz 3x3 (array de arrays)
+var matriz2 = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]
+8. // Definindo uma variável com valor float
+var numero = 3.14
+9. // Definindo um array de caracteres
+var letras = ["a", "b", "c"]
