7467fe85f6b047ac68f29b841966e82b0c96d724
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2024-08-21 20:44:39 -0300

    Tela Inicial - Primeiro App
    Primeiro App em Swift
    App: Lista de Tarefas

---
+++ DesafioApp/Lista de Tarefas/Lista_de_TarefasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lista_de_TarefasApp.swift
+//  Lista de Tarefas
+//
+//  Created by user on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
